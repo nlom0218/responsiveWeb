8e4814377db1e93bf4984aa76a40edf0af9aadee
nlom0218 <nlom0218@naver.com>
2021-10-28 23:15:21 +0900

    settgin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import FluidGrid from './Pages/Chpater1/FluidGrid';
+import { GlobalStyle } from './styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return (<React.StrictMode>
+    <GlobalStyle />
+    <Router>
+      <Switch>
+        <Route path="fluidGrid"><FluidGrid /></Route>
+      </Switch>
+    </Router>
+  </React.StrictMode>);
 }
 
 export default App;
---
+++ src/Pages/Chpater1/FluidGrid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Contatiner = styled.div``
+
+const FluidGrid = () => {
+  return (<Contatiner>
+
+  </Contatiner>);
+}
+
+export default FluidGrid;
