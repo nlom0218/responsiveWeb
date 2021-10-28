cce5e0f37d61d364ba2990bd8479f1b654869e43
nlom0218 <nlom0218@naver.com>
2021-10-28 23:29:49 +0900

    FluidGrid

--- src/Pages/Chpater1/FluidGrid.js
+++ src/Pages/Chpater1/FluidGrid.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+import BackHome from '../../Components/BackHome';
 
 const Contatiner = styled.div``
 
+const Wrap = styled.div`
+width:90%;
+height:500px;
+margin:0 auto;
+background:#e65d5d;
+border:4px solid #000;
+`
+
 const FluidGrid = () => {
   return (<Contatiner>
-
+    <BackHome />
+    <Wrap></Wrap>
   </Contatiner>);
 }
 
