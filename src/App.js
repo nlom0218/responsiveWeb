9360af33707a89494d580e86324722fdd500fe13
nlom0218 <nlom0218@naver.com>
2021-11-01 23:44:09 +0900

    gh pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, HashRouter as Router, Switch } from 'react-router-dom';
 import Home from './Pages/Home';
 import { GlobalStyle } from './styles';
 
